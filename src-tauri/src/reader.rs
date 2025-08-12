df6acc96472a35d6970c372e137ba7228fa3b358
dongchao-1 <dccrazyboy@gmail.com>
2025-08-12 09:11:20 +0800

    feat: 添加docx reader

--- src-tauri/src/reader.rs
+++ src-tauri/src/reader.rs
@@ -3,6 +3,10 @@ use std::collections::HashMap;
 use std::vec;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use tempfile::TempDir;
+use zip::ZipArchive;
+use quick_xml::events::Event as quickXmlEvent;
+use quick_xml::Reader as quickXmlReader;
 
 #[derive(Debug)]
 pub struct Item<'a> {
@@ -23,7 +27,7 @@ pub struct CompositeReader {
 
 impl CompositeReader {
     pub fn new() -> Self {
-        let readers: Vec<Box<dyn Reader>> = vec![Box::new(TxtReader)];
+        let readers: Vec<Box<dyn Reader>> = vec![Box::new(TxtReader), Box::new(DocxReader)];
         let mut reader_map: HashMap<String, Box<dyn Reader>> = HashMap::new();
         for reader in readers {
             reader_map.insert(
@@ -70,6 +74,78 @@ impl Reader for TxtReader {
     }
 }
 
+
+struct DocxReader;
+impl Reader for DocxReader {
+    fn read<'a>(&self, file_path: &'a Path) -> Result<Vec<Item<'a>>, Box<dyn std::error::Error>> {
+        let temp_dir = TempDir::new()?;
+        let file = File::open(file_path)?;
+        let mut archive = ZipArchive::new(file)?;
+        archive.extract(&temp_dir)?;
+
+
+        // 提取 document.xml
+        let document_path = temp_dir.path().join("word/document.xml");
+        let reader = BufReader::new(File::open(document_path)?);
+        let mut xml_reader = quickXmlReader::from_reader(reader);
+
+        let mut txt = String::new();
+        let mut buf = Vec::new();
+        let mut items = vec![];
+        let mut line = 1;
+
+        loop {
+            match xml_reader.read_event_into(&mut buf)? {
+                quickXmlEvent::Start(e) if e.name().as_ref() == b"w:p" => {
+                    if let Some(item) = self.create_item(file_path, &mut txt, &mut line) {
+                        items.push(item);
+                    }
+                }
+                quickXmlEvent::Text(e) => {
+                    txt.push_str(&e.decode()?);
+                }
+                quickXmlEvent::Eof => {
+                    if let Some(item) = self.create_item(file_path, &mut txt, &mut line) {
+                        items.push(item);
+                    }
+                    break;
+                }, // 文件结束
+                _ => (),
+            }
+            buf.clear();
+        }
+
+        Ok(items)
+    }
+
+    fn supports(&self) -> &str {
+        "docx"
+    }
+}
+
+impl DocxReader {
+    fn create_item<'a>(&self, file_path: &'a Path, txt: &mut String, line: &mut u64) -> Option<Item<'a>> {
+        let item = if !txt.trim().is_empty() {
+            let txt_ret = txt.trim().to_string();
+            txt.clear();
+            let line_ret = *line;
+            *line += 1;
+            Some(Item {
+                file: Cow::Borrowed(file_path),
+                page: 0,
+                line: line_ret,
+                content: txt_ret,
+            })
+        } else {
+            None
+        };
+
+        item
+    }
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -94,6 +170,15 @@ mod tests {
         assert_eq!(reader.supports(), "txt");
         let items = reader.read(Path::new("../test_data/1.txt")).unwrap();
         assert_eq!(items.len(), 4);
+    }
+
+    #[test]
+    fn test_docx_reader() {
+        let reader = DocxReader;
+        assert_eq!(reader.supports(), "docx");
+        let items = reader.read(Path::new("../test_data/test.docx")).unwrap();
+        // println!("Items: {:?}", items);
+        assert_eq!(items.len(), 10);
     }
 }
 
